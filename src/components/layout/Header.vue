45e6254bda6e618aefcd78baa6fdba73ff529964
Myroslav <94mir.ms@gmail.com>
2020-08-04 09:42:31 +0300

    app complete

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header class='header'>
+        <h1>Todo list</h1>
+
+        <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    .header {
+        background-color: #333;
+        color: #fff;
+        text-align: center;
+        padding: 10px;
+    }
+
+    .header a {
+        color: #fff;
+        padding-right: 5px;
+        color: #fff;
+        text-decoration: none;
+    }
+</style>
